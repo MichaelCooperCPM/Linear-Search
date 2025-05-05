f2cdd7a6665abb7af1526e6ef1445c9b6e8971ca
MichaelCooperCPM <michael.cooper@education.vic.gov.au>
2025-05-05 19:37:57 +1000

    Complete, with comments

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,21 +36,22 @@
             SearchResultLabel = new Label();
             SearchedValueTextbox = new TextBox();
             LinearSearchButton = new Button();
+            label3 = new Label();
             SuspendLayout();
             // 
             // GenerateArrayButton
             // 
-            GenerateArrayButton.Location = new Point(299, 119);
+            GenerateArrayButton.Location = new Point(320, 119);
             GenerateArrayButton.Name = "GenerateArrayButton";
-            GenerateArrayButton.Size = new Size(161, 67);
+            GenerateArrayButton.Size = new Size(161, 41);
             GenerateArrayButton.TabIndex = 0;
-            GenerateArrayButton.Text = "Generate Array";
+            GenerateArrayButton.Text = "Generate and Display Array";
             GenerateArrayButton.UseVisualStyleBackColor = true;
             GenerateArrayButton.Click += GenerateArrayButton_Click;
             // 
             // ArraySizeTextbox
             // 
-            ArraySizeTextbox.Location = new Point(333, 90);
+            ArraySizeTextbox.Location = new Point(350, 79);
             ArraySizeTextbox.Name = "ArraySizeTextbox";
             ArraySizeTextbox.Size = new Size(100, 23);
             ArraySizeTextbox.TabIndex = 1;
@@ -58,7 +59,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(257, 51);
+            label1.Location = new Point(260, 43);
             label1.Name = "label1";
             label1.Size = new Size(281, 15);
             label1.TabIndex = 2;
@@ -67,15 +68,15 @@
             // DisplayArrayListbox
             // 
             DisplayArrayListbox.FormattingEnabled = true;
-            DisplayArrayListbox.Location = new Point(299, 192);
+            DisplayArrayListbox.Location = new Point(320, 169);
             DisplayArrayListbox.Name = "DisplayArrayListbox";
-            DisplayArrayListbox.Size = new Size(161, 124);
+            DisplayArrayListbox.Size = new Size(161, 154);
             DisplayArrayListbox.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(309, 452);
+            label2.Location = new Point(325, 505);
             label2.Name = "label2";
             label2.Size = new Size(42, 15);
             label2.TabIndex = 4;
@@ -84,33 +85,45 @@
             // SearchResultLabel
             // 
             SearchResultLabel.AutoSize = true;
-            SearchResultLabel.Location = new Point(357, 452);
+            SearchResultLabel.Location = new Point(373, 505);
             SearchResultLabel.Name = "SearchResultLabel";
             SearchResultLabel.Size = new Size(103, 15);
             SearchResultLabel.TabIndex = 5;
             SearchResultLabel.Text = "Search not yet run";
+            SearchResultLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // SearchedValueTextbox
             // 
-            SearchedValueTextbox.Location = new Point(333, 353);
+            SearchedValueTextbox.Location = new Point(350, 407);
             SearchedValueTextbox.Name = "SearchedValueTextbox";
             SearchedValueTextbox.Size = new Size(100, 23);
             SearchedValueTextbox.TabIndex = 6;
             // 
             // LinearSearchButton
             // 
-            LinearSearchButton.Location = new Point(316, 404);
+            LinearSearchButton.Location = new Point(342, 446);
             LinearSearchButton.Name = "LinearSearchButton";
-            LinearSearchButton.Size = new Size(117, 23);
+            LinearSearchButton.Size = new Size(117, 36);
             LinearSearchButton.TabIndex = 7;
             LinearSearchButton.Text = "Run Linear Search";
             LinearSearchButton.UseVisualStyleBackColor = true;
+            LinearSearchButton.Click += LinearSearchButton_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(322, 380);
+            label3.Name = "label3";
+            label3.Size = new Size(157, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Enter the value to search for:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 562);
+            Controls.Add(label3);
             Controls.Add(LinearSearchButton);
             Controls.Add(SearchedValueTextbox);
             Controls.Add(SearchResultLabel);
@@ -135,5 +148,6 @@
         private Label SearchResultLabel;
         private TextBox SearchedValueTextbox;
         private Button LinearSearchButton;
+        private Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,45 +7,55 @@ namespace Linear_Search
             InitializeComponent();
         }
 
-        int[] array;
+        int[] array; // class level array so all methods/functions can access it
 
         private void GenerateArrayButton_Click(object sender, EventArgs e)
         {
-            array = GenerateArray();
-            DisplayArray(array);
+            array = GenerateArray(); // array is set to result of GenerateArray Function
+            DisplayArray(array); // call DisplayArray method, pass it the array
         }
 
         private void LinearSearchButton_Click(object sender, EventArgs e)
         {
-            int searchedValue = GetSearchedValue();
-            bool found = LinearSearch(array, searchedValue);
-            DisplayResult(found);
+            int searchedValue = GetSearchedValue(); // searched value is equal to the value returned by GetSearchValue function
+            bool found = LinearSearch(array, searchedValue); // found is equal to returned value from LinearSearch, passed the array and searched value
+            DisplayResult(found); // Call DisplayRessult, pass it found variable
         }
 
         int[] GenerateArray()
         {
-            int arraySize = Convert.ToInt32(ArraySizeTextbox.Text);
-            int[] array = new int[arraySize];
+            SearchResultLabel.Text = "Search not yet run"; // reset result label
+            int arraySize = Convert.ToInt32(ArraySizeTextbox.Text); // use value from textbox for array length and max value
+            int[] array = new int[arraySize]; // create array with a length of the value entered by user
 
-            Random rand = new Random();
+            Random rand = new Random(); // create a new random number generator called rand
 
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = rand.Next(0, arraySize);
+                array[i] = rand.Next(0, arraySize); // set each value in the array to a random number between 0 and the length of the array
             }
 
-            return array;
+            return array; // return the array to where it was called in GenerateArrayButton_Click method
         }
 
         void DisplayArray(int[] array)
         {
-            DisplayArrayListbox.Items.Clear();
-            foreach (int i in array)
+            DisplayArrayListbox.Items.Clear(); // clear the listbox
+            if (array.Length <= 10000) // only display the array if it has a length less than 10000 (Listbox is slow otherwise)
             {
-                DisplayArrayListbox.Items.Add(i);
+                foreach (int value in array)
+                {
+                    DisplayArrayListbox.Items.Add(value); // add values from the array to the listbox
+                }
             }
+            else
+            {
+                DisplayArrayListbox.Items.Add("Array >10000 values"); // message to display if array length is > 10000
+            }
+
         }
 
+        // Linear search algorithm, returns true or false depending if value passed value is found in passed array
         bool LinearSearch(int[] array, int searchedValue)
         {
             foreach (int i in array)
@@ -60,11 +70,13 @@ namespace Linear_Search
 
         int GetSearchedValue()
         {
+            // return valye from the textbox to where the function was called
             return Convert.ToInt32(SearchedValueTextbox.Text);
         }
 
         void DisplayResult(bool found)
         {
+            // display the result of the serach, based on the value of found
             if (found == true)
             {
                 SearchResultLabel.Text = "Value found";
